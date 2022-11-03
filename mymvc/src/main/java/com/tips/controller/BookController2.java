b79e8ecee7ad96183c4060f28fb69fd5c9b04120
jsostrich <jobjs5877@gmail.com>
2022-11-03 14:17:31 +0900

    오전 mvc 구조 진행 너무 어려우니 꼭 확인해야한다

---
+++ mymvc/src/main/java/com/tips/controller/BookController2.java
@@ -0,0 +1,27 @@
+package com.tips.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.controller.Controller;
+import com.tips.model.BookExpert;
+
+public class BookController2 implements Controller{
+
+	@Override
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		//1
+		String lang=request.getParameter("lang");
+
+		//2
+		BookExpert model = new BookExpert();
+		String result=model.getTip(lang);
+
+		//3
+		request.setAttribute("result", result);
+
+		//4
+		return "/tips/bookResult.jsp";
+	}
+
+}
