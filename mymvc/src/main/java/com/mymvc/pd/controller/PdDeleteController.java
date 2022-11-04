50dc27205b1902f1d9ecf9474b78030d99550074
jsostrich <jobjs5877@gmail.com>
2022-11-04 14:25:32 +0900

    커몬 폴더 카피및 dele작성진행

---
+++ mymvc/src/main/java/com/mymvc/pd/controller/PdDeleteController.java
@@ -0,0 +1,53 @@
+package com.mymvc.pd.controller;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.controller.Controller;
+import com.mymvc.pd.model.PdDTO;
+import com.mymvc.pd.model.PdService;
+
+public class PdDeleteController implements Controller {
+
+	@Override
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+
+		/*
+		  [7]삭제 - delete
+		  
+		  /pd/pdDelete.do => DispatchServlet =>PdDeleteController => 
+		   => 1)pdList.do 로 redirect 또는 
+		   	  2)메시지를 보여주는 경우 message.jsp로 포워드
+		 */
+		
+		String no = request.getParameter("no");
+		
+		PdService service = new PdService();
+		String msg="", url="";
+		try {
+			int cnt = service.deletePd(Integer.parseInt(no));
+			if(cnt>0) {
+				msg="상품 삭제 성공";
+				url="/pd/pdList.do";
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+		request.setAttribute("msg", msg);
+		
+		
+		
+		
+		return null;
+	}
+
+	@Override
+	public boolean isRedirect() {
+
+		return false;
+	}
+
+}
