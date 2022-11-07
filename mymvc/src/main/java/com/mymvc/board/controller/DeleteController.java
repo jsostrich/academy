e0b0fa3ad85bde403dd1bccee2a8dc0287f00462
jsostrich <jobjs5877@gmail.com>
2022-11-07 12:59:57 +0900

    d

--- mymvc/src/main/java/com/mymvc/board/controller/DeleteController.java
+++ mymvc/src/main/java/com/mymvc/board/controller/DeleteController.java
@@ -8,15 +8,30 @@ import com.controller.Controller;
 public class DeleteController implements Controller{
 
 	@Override
-	public String requestProcess(HttpServletRequest request, 
-			HttpServletResponse response) throws Throwable {
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		/*
+		 [8] 삭제화면
+			/board/delete.do => DispatcherServlet => DeleteController
+			=> delete.jsp 로 포워드
+		*/
+		//1
+		String no = request.getParameter("no");
+		if(no ==null || no.isEmpty()) {
+			request.setAttribute("msg", "잘못된 url입니다");
+			request.setAttribute("url", "/board/list.do");
+
+			return "/common/message.jsp";
+		}
 		
+		//2
+		//3
+		//4
 		return "/board/delete.jsp";
+
 	}
 
 	@Override
 	public boolean isRedirect() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
--- mymvc/src/main/java/com/mymvc/board/controller/DeleteOkController.java
+++ mymvc/src/main/java/com/mymvc/board/controller/DeleteOkController.java
@@ -11,39 +11,48 @@ import com.mymvc.board.model.BoardService;
 public class DeleteOkController implements Controller{
 
 	@Override
-	public String requestProcess(HttpServletRequest request, 
-			HttpServletResponse response) throws Throwable {
-		
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		/*
+		 [9] 삭제처리 - delete
+			/board/delete_ok.do => DispatcherServlet => DeleteOkController
+			1) => list.do 로 redirect
+			또는
+			2) 메시지 띄우는 경우
+			message.jsp로 포워드
+		 */
+		//1
 		String no = request.getParameter("no");
 		String pwd = request.getParameter("pwd");
-		
-		BoardService service = new BoardService();
-		
-		String msg = "삭제 실패", url="/board/delete.jsp?no="+no;
+
+		//2
+		BoardService boardService = new BoardService();
+
+		String msg="글 삭제 실패",url="/board/delete.do?no="+no;
 		try {
-			if(service.checkPwd(Integer.parseInt(no), pwd)) {
-				int cnt = service.deleteBoard(Integer.parseInt(no));
+			if(boardService.checkPwd(Integer.parseInt(no), pwd)) {
+				int cnt = boardService.deleteBoard(Integer.parseInt(no));
 				if(cnt>0) {
-					msg="삭제 성공";
+					msg="글 삭제되었습니다";
 					url="/board/list.do";
 				}
 			}else {
-				msg="비밀번호부터 맞추세요";
+				msg="비밀번호가 일치하지않습니다.";
 			}
+
 		}catch (SQLException e) {
 			e.printStackTrace();
-			// TODO: handle exception
 		}
 		
+		//3
 		request.setAttribute("msg", msg);
 		request.setAttribute("url", url);
 		
+		//4
 		return "/common/message.jsp";
 	}
 
 	@Override
 	public boolean isRedirect() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
